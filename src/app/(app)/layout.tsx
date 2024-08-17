95c5c29d1c6a04c07eb94dcf14de6eae5dc13377
Shin.M <synornithe@gmail.com>
2024-08-17 16:56:07 +0900

    fix: loadingの使用停止

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -1,16 +1,16 @@
 import * as Layout from '@/components/app/layouts';
-import { Suspense } from 'react';
+// import { Suspense } from 'react';
 
 const AppLayout = ({ children }: { children: React.ReactNode }) => {
     return (
-        <Suspense fallback={<Layout.Loading />}>
+        // <Suspense fallback={<Layout.Loading />}>
             <div className="min-h-screen w-full bg-app-base font-mPlusRounded text-app-text-main">
                 <Layout.Header />
                 <div className="relative z-0">{children}</div>
                 <Layout.Footer />
                 <Layout.ScrollToTop />
             </div>
-        </Suspense>
+        // </Suspense>
     );
 };
 
