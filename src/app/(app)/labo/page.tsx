de9b82e7e7f5506483a54de255dc306406ec04c0
Shin.M <synornithe@gmail.com>
2024-08-18 23:24:45 +0900

    fix: laboページ
    - laboCardコンポーネント内にgsap導入
    - リンクを別タブ開示対応
    - コンポーネントへの引数を追加
    - 項目１つ追加

--- src/app/(app)/labo/page.tsx
+++ src/app/(app)/labo/page.tsx
@@ -3,42 +3,55 @@ import { Body } from '@/components/app/layouts';
 import { Metadata } from 'next';
 import { StaticImageData } from 'next/image';
 import birdImg from '~/public/bird.jpeg';
+import devImg from '~/public/developing.jpeg';
 import noImage from '~/public/NoImage.png';
 
 export const metadata: Metadata = {
     title: 'labo',
 };
 
-// TODO: create labo table
 const LaboListContents: Array<{
     id: number;
     title: string;
     cardBody: string;
     cardImg: StaticImageData;
     link: string;
+    newTab: boolean;
 }> = [
     {
         id: 1,
-        title: '野鳥出現予測アプリ',
-        cardBody: '天候と野鳥確認事例数より出現の予測をするアプリです',
+        title: '野鳥出現予測アプリ（開発中）',
+        cardBody: '天候と野鳥確認事例数より出現の予測をするアプリです. ',
         cardImg: birdImg,
         link: '/labo/eBird',
+        newTab: false,
     },
     {
         id: 2,
-        title: '自由編集アプリ',
+        title: 'タスク管理ツール',
         cardBody:
-            '決められた枠内に自由にテキストや画像を入れ込み、保存できるアプリです',
-        cardImg: noImage,
-        link: '/labo/fabric',
+            'CSVデータから受注データを管理し、作業者へタスクを振り分けする社内アプリです.',
+        cardImg: devImg,
+        link: 'https://github.com/syn154045/task_manage_system',
+        newTab: true,
     },
     {
         id: 3,
-        title: '運転免許証の車両認証システム',
+        title: '自由編集アプリ（開発中）',
         cardBody:
-            '車の盗難防止のために、運転免許証が無いと車両を起動できないようにするシステムのプロトタイプですなんちゃってのシステムではない将来性の高いものと自負しているんですが',
-        cardImg: noImage,
+            '決められた枠内に自由にテキストや画像を入れ込み、保存できるアプリです',
+        cardImg: devImg,
+        link: '/labo/fabric',
+        newTab: false,
+    },
+    {
+        id: 4,
+        title: '運転免許証の車両認証システム（要件定義中）',
+        cardBody:
+            '車の盗難防止のために、運転免許証が無いと車両を起動できないようにするシステム（組込系のソフト部分）',
+        cardImg: devImg,
         link: '/labo',
+        newTab: false,
     },
 ];
 
@@ -53,6 +66,7 @@ const Labo = () => {
                         cardImg={content.cardImg}
                         link={content.link}
                         key={content.id}
+                        newTab={content.newTab}
                     />
                 ))}
             </div>
--- src/components/app/elements/cards/_LaboListCard.tsx
+++ src/components/app/elements/cards/_LaboListCard.tsx
@@ -1,10 +1,16 @@
+'use client';
+
+import gsap from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
 import Image, { StaticImageData } from 'next/image';
+import { useEffect, useRef } from 'react';
 
 interface LaboListCardProps {
     title: string;
     cardBody: string;
     cardImg: StaticImageData;
     link: string;
+    newTab: boolean;
 }
 
 const LaboListCard = ({
@@ -12,10 +18,37 @@ const LaboListCard = ({
     cardBody,
     cardImg,
     link,
+    newTab,
 }: LaboListCardProps) => {
+    const cardRef = useRef(null);
+    gsap.registerPlugin(ScrollTrigger);
+
+    useEffect(() => {
+        const element = cardRef.current;
+
+        gsap.to(element, {
+            opacity: 1,
+            duration: 10,
+            scrollTrigger: {
+                trigger: element,
+                scrub: true,
+                start: 'top 80%',
+                end: 'top 70%',
+            },
+        });
+    }, []);
+
     return (
-        <div className="flex w-full flex-col rounded-3xl border-[0.125rem] border-app-accent2 bg-app-main/30 transition-all duration-300 hover:bg-app-accent2/30">
-            <a href={link} className="group block">
+        <div
+            ref={cardRef}
+            className="flex w-full flex-col rounded-3xl border-[0.125rem] border-app-accent2 bg-app-main/30 opacity-0 transition-all duration-300 hover:bg-app-accent2/30"
+        >
+            <a
+                href={link}
+                className="group block"
+                target={newTab ? '_blank' : '_self'}
+                rel={newTab ? 'noopener noreferrer' : ''}
+            >
                 <Image
                     src={cardImg}
                     alt="labos"
